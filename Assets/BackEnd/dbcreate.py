3aa73a5f539ee3504c875abbbee95ae685019100
EvilFlame112 <rampranav25@gmail.com>
2021-08-03 16:44:44 +0530

    Update dbcreate.py

--- Assets/BackEnd/dbcreate.py
+++ Assets/BackEnd/dbcreate.py
@@ -15,6 +15,5 @@ def chkdb(db):
 def makedb():
     cur.execute("CREATE DATABASE hospitaldata")
     cur.execute("use hospitaldata")
-    cur.execute("CREATE TABLE patient(PatientID int PRIMARY KEY, Name varchar(50), Age tinyint, Ailment varchar(50), Payment type varchar(10), Payment status varchar(10), Contact decimal(10,0), Amount decimal(10,2))")
-    cur.execute("CREATE TABLE doctor(DoctorID int PRIMARY KEY, Name varchar(50), Ailment varchar(50), Payment type varchar(10), Payment status varchar(10), Contact decimal(10,0), Amount decimal(10,2))")
+    cur.execute("CREATE TABLE patient(PatientID int PRIMARY KEY, Name varchar(50), Age int, Ailment varchar(50), Payment type varchar(10), Payment status varchar(10), Contact decimal(10,0), Amount decimal(10,2))")
 
