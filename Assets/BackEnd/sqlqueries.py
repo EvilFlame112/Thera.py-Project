8c74992c68f68afde023da9ba83d9b7575e4503f
SaiTarun9000 <85156402+SaiTarun9000@users.noreply.github.com>
2021-08-04 10:08:35 +0530

    sql queries part

--- Assets/BackEnd/sqlqueries.py
+++ Assets/BackEnd/sqlqueries.py
@@ -1,6 +1,17 @@
 import mysql.connector
 conn=mysql.connector.connect(host='localhost', user="root",  passwd="", database="hospitalmgmt")
 cur=conn.cursor
-def addtotable(tablename, PID, Name, age, Ailment, paymentT, status, contact, amount):
-    cur.execute(f"INSERT INTO {tablename} values({PID}, {Name}, {age}, {Ailment}, {paymentT}, {status}, {contact}, {amount})")
+def addtopatient( pid, namep, age, ailment, paytypep, status, contactp, amountp):
+    cur.execute(f"INSERT INTO patient values({pid}, {namep}, {age}, {ailment}, {paytypep}, {status}, {contactp}, {amountp})")
     conn.commit()
+def addtodoctor(did,named,specialization,dojd,contactd,salaryd):
+    cur.execute(f"insert into doctor values({did},{named},{specialization},{dojd},{contactd},{salaryd}) ")
+    conn.commit()
+def addtonurse(nid,namen,department,dojn,contactn,salaryn):
+    cur.execute(f"insert into nurse values({nid},{namen},{department},{dojn},{contactn},{salaryn})")
+    conn.commit()
+def addtoemployee(eid,namee,job,paytypee,contacte,salarye):
+    cur.execute(f"insert into employee values({eid},{namee},{job},{paytypee},{contacte},{salarye}")
+    conn.commit()
+def dele(tablename,id,idinput):
+    cur.execute(f"delete from {tablename} where {id}={idinput}")
