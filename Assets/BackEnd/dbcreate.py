c9d1dbc914b72a16bd5360bea12f645ba966963d
SaiTarun9000 <85156402+SaiTarun9000@users.noreply.github.com>
2021-08-04 10:43:01 +0530

    dbcreate updated

--- Assets/BackEnd/dbcreate.py
+++ Assets/BackEnd/dbcreate.py
@@ -1,5 +1,5 @@
 import mysql.connector
-conn=mysql.connector.connect(host='localhost', user="root",  passwd="")
+conn=mysql.connector.connect(host='localhost', user="root",  passwd="tar$1402")
 cur = conn.cursor()
 def chkdb(db):
     cur.execute("show databases;")
@@ -15,5 +15,9 @@ def chkdb(db):
 def makedb():
     cur.execute("CREATE DATABASE hospitaldata")
     cur.execute("use hospitaldata")
-    cur.execute("CREATE TABLE patient(PatientID int PRIMARY KEY, Name varchar(50), Age int, Ailment varchar(50), Payment type varchar(10), Payment status varchar(10), Contact decimal(10,0), Amount decimal(10,2))")
+    cur.execute("create table patient(Pateint_Id int PRIMARY KEY, Name varchar(50), Age int, Ailment varchar(50), Payment_Type varchar(10), Payment_Status varchar(50), Contact decimal(10,0), Amount decimal(10,2))")
+    cur.execute("create table doctor(Doctor_Id int primary key,Name varchar(50),Specialization varchar(50),DOJ date,Contact bigint, Salary Decimal(10,2)) ")
+    cur.execute("create table nurse(Nurse_Id int primary key,Name varchar(50),Department varchar(50),DOJ date, Contact bigint,Salary decimal(10,2))")
+    cur.execute("create table employee(Employee_Id int primary key,Name varchar(50),Job VARCHAR(50),Payment_Type varchar(50),Contact bigint,Salary Decimal(10,2))")
 
+makedb()
