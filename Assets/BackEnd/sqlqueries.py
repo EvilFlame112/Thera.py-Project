db1b9e8a735642b04e43cf17d35194c06a5e26b7
SaiTarun9000 <85156402+SaiTarun9000@users.noreply.github.com>
2021-10-11 15:50:25 +0530

    chdoctor update

--- Assets/BackEnd/sqlqueries.py
+++ Assets/BackEnd/sqlqueries.py
@@ -17,9 +17,14 @@ with open(secret_path, "r") as secret:
 
 conn=mysql.connector.connect(host=vals[0], user=vals[1],  passwd=fernet.decrypt(eval(vals[2])).decode(), database="hospitaldata")
 cur=conn.cursor()
-def addtopatient(pid, namep, age, ailment, paytypep, status, contactp, amountp):
-    cur.execute(f"INSERT INTO patient values({pid}, '{namep}', {age}, '{ailment}', '{paytypep}', '{status}', {contactp}, {amountp})")
+def addtopatient(pid, namep, age, ailment,admsts, paytypep, status, contactp, amountp):
+    cur.execute(f"INSERT INTO patient values({pid}, '{namep}', {age}, '{ailment}','{admsts}', '{paytypep}', '{status}', {contactp}, {amountp})")
     conn.commit()
+
+
+
+
+
 def addtodoctor(did,named,specialization,dojd,contactd,salaryd):
     cur.execute(f"insert into doctor values({did},'{named}','{specialization}','{dojd}',{contactd},{salaryd}) ")
     conn.commit()
@@ -29,12 +34,22 @@ def addtonurse(nid,namen,department,dojn,contactn,salaryn):
 def addtoemployee(eid,namee,job,doje,contacte,salarye):
     cur.execute(f"insert into employee values({eid},'{namee}','{job}','{doje}',{contacte},{salarye})")
     conn.commit()
+def addtopharmacy(itcd,Mname,QTS,MRP):
+    cur.execute(f"insert into pharmacy values({itcd},{Mname},{QTS},{MRP})")
+    conn.commit()
+def addtochdoctor(slno,chid,name, sp, dojch,contact,salary):
+    cur.execute(f"insert into chdoctor values({slno},{chid},{name},{sp},{dojch},{contact},{salary})")
+    conn.commit()
+
+
 def dele(tablename,id,idinput):
     cur.execute(f"delete from {tablename} where {id}={idinput}")
     conn.commit()
 def purgeP():
     cur.execute("drop table patient")
-    cur.execute("CREATE TABLE IF NOT EXISTS patient(Patient_Id int PRIMARY KEY, Name varchar(50), Age int, Ailment varchar(50), Payment_Type varchar(10), Payment_Status varchar(50), Contact decimal(10,0), Amount decimal(10,2))")
+    cur.execute("drop table chdoctor")
+    cur.execute("CREATE TABLE IF NOT EXISTS patient(Patient_Id int PRIMARY KEY, Name varchar(50), Age int, Ailment varchar(50),Admission_Status varchar(11), Payment_Type varchar(10), Payment_Status varchar(50), Contact decimal(10,0), Amount decimal(10,2))")
+    cur.execute("CREATE TABLE IF NOT EXISTS chdoctor(Slno int PRIMARY KEY,FOREIGN KEY (Ch_Id) REFERENCES patient(Patient_Id),Name varchar(50),Specialization varchar(50),DOJ date,Contact bigint, Salary decimal(10,2)) ") 
 def purgeD():
     cur.execute("drop table doctor")
     cur.execute("CREATE TABLE IF NOT EXISTS doctor(Doctor_Id int primary key,Name varchar(50),Specialization varchar(50),DOJ date,Contact bigint, Salary decimal(10,2)) ")
@@ -44,6 +59,11 @@ def purgeN():
 def purgeE():
     cur.execute("drop table employee")
     cur.execute("CREATE TABLE IF NOT EXISTS employee(Employee_Id int primary key,Name varchar(50),Job VARCHAR(50),DOJ date,Contact bigint,Salary decimal(10,2))")
+def purgePh():
+    cur.execute("drop table pharmacy")
+     cur.execute("CREATE TABLE IF NOT EXISTS pharmacy(Item_Code int primary key, Medicine_Name varchar(50), QIS int, MRP decimal(9,2) ")
+
+
 def query(tablename):
     cur.execute(f"SELECT * FROM {tablename}")
     return cur.fetchall()
@@ -56,8 +76,8 @@ def closeconnection(main):
     conn.close()
     main.destroy()
 
-def uppatient(pid1, namep1, age1, ailment1, paytypep1, status1, contactp1, amountp1):
-    cur.execute(f"update patient set Name='{namep1}',Age={age1},Ailment='{ailment1}',Payment_Type='{paytypep1}',Payment_Status='{status1}',Contact={contactp1},Amount={amountp1} Where Patient_Id={pid1}")
+def uppatient(pid1, namep1, age1, ailment1,admsts, paytypep1, status1, contactp1, amountp1):
+    cur.execute(f"update patient set Name='{namep1}',Age={age1},Ailment='{ailment1}',Admission_Status='{admsts}',Payment_Type='{paytypep1}',Payment_Status='{status1}',Contact={contactp1},Amount={amountp1} Where Patient_Id={pid1}")
     conn.commit()
 def updoctor(did1,named1,specialization1,dojd1,contactd1,salaryd1):
     cur.execute(f"update doctor set Name='{named1}',Specialization='{specialization1}',DOJ='{dojd1}',Contact={contactd1},Salary={salaryd1} where Doctor_Id={did1}")
@@ -67,4 +87,10 @@ def upnurse(nid1,namen1,department1,dojn1,contactn1,salaryn1):
     conn.commit()
 def upemployee(eid1,namee1,job1,doje1,contacte1,salarye1):
     cur.execute(f"update employee set Name='{namee1}',Job='{job1}',DOJ='{doje1}',Contact={contacte1}, Salary={salarye1} where Employee_Id={eid1}")
+    conn.commit()
+def uppharmacy(itcd1,Mname1,QTS1,MRP1):
+    cur.execute(f"update pharmacy set Item_Code={itcd1},Medicine_Name='{Mname1}',QTS={QTS1},MRP={MRP1}")
+    conn.commit()
+def upchdoctor(slno1,chid1,name1, sp1, dojch1,contact1,salary1):
+    cur.execute(f"update chdoctor set Ch_Id={chid1},Name='{name1}',Specialization={sp1}, DOJ={dojch1}, Contact ={contact1},salary={salary1} where Slno={slno1}")
     conn.commit()
